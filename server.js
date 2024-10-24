ba79e8cc2105a751399374e204ccbfacd99a82bc
ytAbdelrazik <yt_abdelrazik@outlook.com>
2024-10-24 03:03:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const app = express();
 app.use(express.json());
 
+
 // MongoDB URI
 const dbURI = 'mongodb+srv://ashrafahmed2006:dsberlin@cluster0.rfhbl.mongodb.net/Students';
 
